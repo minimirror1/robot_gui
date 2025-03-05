3ce3e20290c9974c513c53f1a5f51f2b72d218ab
shin ho sick <minimirror1@gmail.com>
2025-03-05 09:36:21 +0900

    Initial commit

---
+++ robot_gui_pkg/robot_gui.py
@@ -0,0 +1,14 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+def main():
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+    window.setWindowTitle("Robot GUI")
+    window.resize(800, 600)
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'robot_gui_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='shs',
+    maintainer_email='minimirror1@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'robot_gui = robot_gui_pkg.robot_gui:main'
+        ],
+    },
+)
